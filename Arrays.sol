4ba4f073d2089636f5834793a8e06d230c65bc50
Sneha Thakare <snhthakare@gmail.com>
2022-03-02 13:50:49 +0530

    variables and arrays

---
+++ Arrays.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.6.0;
+
+contract MyContract {
+    // Arrays
+    uint256[] public uintArray = [1, 2, 3];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string[] public values;
+    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
+
+    function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+    function valueCount() public view returns (uint256) {
+        return values.length;
+    }
+}
